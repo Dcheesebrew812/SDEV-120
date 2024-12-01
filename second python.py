eaa25fc3e45450f29080e5f1c34ea8c8cae46819
Dcheesebrew812 <damoncheesebrew@aol.com>
2024-12-01 10:29:39 -0500

    Add files via upload

---
+++ second python.py
@@ -0,0 +1,5 @@
+print("hello world.")
+print("i just wrote my first python program")
+print("this is so much fun")
+name =input("hello what is your name?") #gets the user's name\
+print("welcome, its so nice to meet you" +name)#prints a message with the users name
